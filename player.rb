cd2df6f95227c7e8945f41136424137a02c69e2f
Khabizat Sharapudinova <habizat22@gmail.com>
2022-06-27 20:58:22 +0000

    Update methods

--- player.rb
+++ player.rb
@@ -5,19 +5,4 @@ class Player
     @name = name
     @lives = 3
   end
-
-  def decrease_lives
-    @lives - 1
-  end
-
-  def alive?
-    @lives > 0
-  end
 end
-
-#Test Code
-player1 = Player.new('Kabby')
-puts player1.name  #'Kabby'
-puts player1.lives #3
-puts player1.decrease_lives #2
-puts player1.alive? #true
